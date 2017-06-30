7e3dde135add1b79ed167195ba2bb7c07cad8991
Davi Morales <morales@localhost.localdomain>
2017-06-30 17:22:07 -0300

    some instructions were corrected in target code

--- target.c
+++ target.c
@@ -59,48 +59,46 @@ void print_target_code(list_instructions *instructions_list){
     fprintf(file_target_code, "\t \t instructionsRAM[%d] = 32'b", instruction->line);
     switch (instruction->type) {
       case G_ADD:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//ADD r[%d],r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "000000%s%s%s00000000000;//ADD r[%d],r[%d] to r[%d]\n",
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
         instruction->register_a, instruction->register_b, instruction->register_c);
       break;
       case G_ADDI:
-        fprintf(file_target_code, "%s%s%s%s;//ADDi r[%d], #%d to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
-        decimal_to_binary(instruction->register_a, 5),
+        fprintf(file_target_code, "000001%s%s%s;//ADDi r[%d], #%d to r[%d]\n",
         decimal_to_binary(instruction->register_c, 5),
+        decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 16),
         instruction->register_a, instruction->immediate, instruction->register_c);
       break;
       case G_SUB:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//SUB r[%d],r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "000010%s%s%s00000000000;//SUB r[%d],r[%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
         instruction->register_a, instruction->register_b, instruction->register_c);
       break;
       case G_SUBI:
-        fprintf(file_target_code, "%s%s%s%s;//SUBi r[%d], #%d to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
-        decimal_to_binary(instruction->register_a, 5),
+        fprintf(file_target_code, "000011%s%s%s;//SUBi r[%d], #%d to r[%d]\n",
+
         decimal_to_binary(instruction->register_c, 5),
+        decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 16),
         instruction->register_a, instruction->immediate, instruction->register_c);
       break;
       case G_MUL:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//TIMES r[%d],r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "000100%s%s%s00000000000;//TIMES r[%d],r[%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
         instruction->register_a, instruction->register_b, instruction->register_c);
       break;
       case G_DIV:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//DIVIDE r[%d],r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "000101%s%s%s00000000000;//DIVIDE r[%d],r[%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
@@ -113,16 +111,16 @@ void print_target_code(list_instructions *instructions_list){
         fprintf(file_target_code, "forgot %d\n", instruction->type);
       break;
       case G_AND:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//AND r[%d],r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "001000%s%s%s00000000000;//AND r[%d],r[%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
         instruction->register_a, instruction->register_b, instruction->register_c);
       break;
       case G_OR:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//OR r[%d],r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "001001%s%s%s00000000000;//OR r[%d],r[%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
@@ -135,8 +133,8 @@ void print_target_code(list_instructions *instructions_list){
         fprintf(file_target_code, "forgot %d\n", instruction->type);
       break;
       case G_NOT:
-        fprintf(file_target_code, "%s%s%s0000000000000000;//NOT r[%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "001101%s%s0000000000000000;//NOT r[%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_c, 5),
         instruction->register_a, instruction->register_c);
@@ -148,14 +146,14 @@ void print_target_code(list_instructions *instructions_list){
         fprintf(file_target_code, "forgot %d\n", instruction->type);
       break;
       case G_PBC:
-        fprintf(file_target_code, "%s%s000000000000000000000;//Pre Branch r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "011111%s000000000000000000000;//Pre Branch r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         instruction->register_a);
       break;
       case G_BOZ:
-        fprintf(file_target_code, "%s%s;//Branch on Zero #%d\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "010011%s;//Branch on Zero #%d\n",
+
         decimal_to_binary(instruction->immediate, 26),
         instruction->immediate);
       break;
@@ -163,54 +161,54 @@ void print_target_code(list_instructions *instructions_list){
         fprintf(file_target_code, "forgot %d\n", instruction->type);
       break;
       case G_JMP:
-        fprintf(file_target_code, "%s%s;//Jump to #%d\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "010101%s;//Jump to #%d\n",
+
         decimal_to_binary(instruction->immediate, 26),
         instruction->immediate);
       break;
       case G_JMPR:
-        fprintf(file_target_code, "%s%s%s;//Jump to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "100011%s%s;//Jump to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->register_a);
       break;
       case G_NOP:
-        fprintf(file_target_code, "%s00000000000000000000000000;//Nop\n",
+        fprintf(file_target_code, "01101100000000000000000000000000;//Nop\n",
         decimal_to_binary(instruction->type, 6));
       break;
       case G_SLT:
-        fprintf(file_target_code, "%s%s%s%s00000000000;//SLT if r[%d] < r[%d], r[%d] = 1 else r[%d] = 0\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "010111%s%s%s00000000000;//SLT if r[%d] < r[%d], r[%d] = 1 else r[%d] = 0\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_b, 5),
         decimal_to_binary(instruction->register_c, 5),
         instruction->register_a, instruction->register_b, instruction->register_c, instruction->register_c);
       break;
       case G_LD:
-        fprintf(file_target_code, "%s%s%s;//Load m[#%d] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "011000%s%s;//Load m[#%d] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->immediate, instruction->register_a);
       break;
       case G_ST:
-        fprintf(file_target_code, "%s%s%s;//Store r[%d] in m[#%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "011001%s%s;//Store r[%d] in m[#%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->register_a, instruction->immediate);
       break;
       case G_LDI:
-        fprintf(file_target_code, "%s%s%s;//Loadi #%d to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "011010%s%s;//Loadi #%d to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->immediate, instruction->register_a);
       break;
       case G_IN:
-        fprintf(file_target_code, "%s%s%s;//Input to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "011101%s%s;//Input to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->register_a);
@@ -219,30 +217,30 @@ void print_target_code(list_instructions *instructions_list){
         fprintf(file_target_code, "forgot %d\n", instruction->type);
       break;
       case G_POUT:
-        fprintf(file_target_code, "%s%s%s;//Pre Output r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "011110%s%s;//Pre Output r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->register_a);
       break;
       case G_OUT:
-        fprintf(file_target_code, "%s%s%s;//Output r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "100000%s%s;//Output r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->immediate, 21),
         instruction->register_a);
       break;
       case G_LDR:
-        fprintf(file_target_code, "%s%s%s%s;//Loadr m[r[%d]] to r[%d]\n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "100001%s%s%s;//Loadr m[r[%d]] to r[%d]\n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_c, 5),
         decimal_to_binary(instruction->immediate, 16),
         instruction->register_a, instruction->register_c);
       break;
       case G_STR:
-        fprintf(file_target_code, "%s%s%s%s;//Storer to r[%d] in m[r[%d]] \n",
-        decimal_to_binary(instruction->type, 6),
+        fprintf(file_target_code, "100010%s%s%s;//Storer to r[%d] in m[r[%d]] \n",
+
         decimal_to_binary(instruction->register_a, 5),
         decimal_to_binary(instruction->register_c, 5),
         decimal_to_binary(instruction->immediate, 16),
